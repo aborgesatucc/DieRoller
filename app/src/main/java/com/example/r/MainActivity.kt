eedb2e3634ce3ea5a18d4a551430a63544246beb
Antonio Borges <antonio.borges@opendoor.com>
2023-02-18 07:52:14 -0500

    Revert back to original commit

--- app/src/main/java/com/example/r/MainActivity.kt
+++ app/src/main/java/com/example/r/MainActivity.kt
@@ -15,8 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.r.ui.DieRollViewModel
-import com.example.r.ui.DieRollerApp
 import com.example.r.ui.theme.RTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,3 +26,50 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun DieRollerApp() {
+    DieImageWithButton()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DieRollerApp()
+}
+
+@Composable
+fun DieImageWithButton() {
+    var roll by remember { mutableStateOf(1) }
+    Column {
+        when (roll) {
+            1 -> Image(
+                painter = painterResource(id = R.drawable.d1),
+                contentDescription = stringResource(id = R.string.d1_desc)
+            )
+            2 -> Image(
+                painter = painterResource(id = R.drawable.d2),
+                contentDescription = stringResource(id = R.string.d2_desc)
+            )
+            3 -> Image(
+                painter = painterResource(id = R.drawable.d3),
+                contentDescription = stringResource(id = R.string.d3_desc)
+            )
+            4 -> Image(
+                painter = painterResource(id = R.drawable.d4),
+                contentDescription = stringResource(id = R.string.d4_desc)
+            )
+            5 -> Image(
+                painter = painterResource(id = R.drawable.d5),
+                contentDescription = stringResource(id = R.string.d5_desc)
+            )
+            6 -> Image(
+                painter = painterResource(id = R.drawable.d6),
+                contentDescription = stringResource(id = R.string.d6_desc)
+            )
+        }
+
+        Button(onClick = { roll = (1..6).random() }) {
+            Text(" Roll ")
+        }
+    }
+}
